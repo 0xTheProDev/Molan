0587a53d568ad4784b77b10ce8fd8e9677392263
Progyan Bhattacharya <progyanb@acm.org>
2018-02-12 21:26:10 +0530

    Basic Setup: Create Fully Functioning REST API
    Installed required modules and created project tree to start working. REST API is fully functioning and live status at http://localhost:3000/status
    
    Signed-off-by: Progyan Bhattacharya <progyanb@acm.org>

---
+++ app.py
@@ -0,0 +1,38 @@
+# Molan: Molan-API
+# Copyright 2018 Tech-Mantra, All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>
+
+# Import Flask Packages
+from flask import Flask
+from flask_restful import Api
+from flask_cors import CORS
+
+# Import Utility Functions and Configurations
+from Util import Config
+
+# Import Controller Modules
+from Controller.Status import Status
+
+# Define Flask Application
+app = Flask(__name__)
+CORS(app)
+
+# REST API from Application
+api = Api(app)
+api.add_resource(Status, Config.API_PATH + "/status")
+
+# Driver Program
+if __name__ == "__main__":
+    app.run(debug=True, host=Config.API_CONF["host"], port=Config.API_CONF["port"], threaded=True)
+    # ssl_context="adhoc"
