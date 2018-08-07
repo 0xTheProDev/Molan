355a8b65bf2033a86080bd3cc79eb0f01b3b7e55
Progyan Bhattacharya <progyanb@acm.org>
2018-08-07 13:16:35 +0530

    App: Configuration of React App
    Set default header to axios and created global state storage in redux.
    
    Signed-off-by: Progyan Bhattacharya <progyanb@acm.org>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from 'container/App';
-import registerServiceWorker from 'util/registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import reducer from "reducer";
+import { NotificationContainer } from "react-notifications";
+import "react-notifications/lib/notifications.css";
+import "semantic-ui-css/semantic.min.css";
+import App from "container/App";
+import registerServiceWorker from "util/registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+/**
+ * Axios Configuration
+ */
+axios.defaults.baseURL = "/api";
+axios.defaults.headers.common["Content-Type"] = "application/json";
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = window.location.host;
+axios.defaults.xsrfHeaderName = "X-CSRFToken";
+
+
+/**
+ * Globa; State Configuration
+ */
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(createStore);
+const store = createStoreWithMiddleware(reducer);
+
+
+/**
+ * DOM Injection
+ */
+ReactDOM.render(
+    <Provider store={store}>
+        <main id="app_container">
+            <NotificationContainer/>
+            <App/>
+        </main>
+    </Provider>
+,document.getElementById("root"));
+
 registerServiceWorker();
