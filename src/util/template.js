bed976b5766e1fa89c4124ebe31488349143d753
Progyan Bhattacharya <progyanb@acm.org>
2018-11-23 12:47:51 +0530

    Language: Add Support for MIPS and Verilog
    API Supported by MIPS Simulator 0.4 and iVerilog 10.0
    
    Signed-off-by: Progyan Bhattacharya <progyanb@acm.org>

--- src/util/template.js
+++ src/util/template.js
@@ -31,10 +31,25 @@ const py_template =
 `# Write Codes Here
 `;
 
+const mips_template =
+`    .text
+main:
+    # Your Codes Here
+`;
+
+const ver_template =
+`/** Workbench Module */
+module wb;
+    /** Main Simulation Here */
+endmodule
+`;
+
 export default [
-    { lang: 'c',       code: c_template,    name: 'C (GCC 6.3)',         ext: '.c' },
-    { lang: 'cpp',     code: cpp_template,  name: 'C++ (G++ 6.3)',       ext: '.cpp' },
-    { lang: 'java',    code: java_template, name: 'Java (OpenJDK 0.8)',  ext: '.java'},
-    { lang: 'python',  code: py_template,   name: 'Python (Python 2.7)', ext: '.py' },
-    { lang: 'python3', code: py_template,   name: 'Python (Python 3.6)', ext: '.py' }
+    { lang: 'c',       code: c_template,    name: 'C (GCC 6.3)',             ext: '.c' },
+    { lang: 'cpp',     code: cpp_template,  name: 'C++ (G++ 6.3)',           ext: '.cpp' },
+    { lang: 'java',    code: java_template, name: 'Java (OpenJDK 0.8)',      ext: '.java'},
+    { lang: 'python',  code: py_template,   name: 'Python (Python 2.7)',     ext: '.py' },
+    { lang: 'python3', code: py_template,   name: 'Python (Python 3.6)',     ext: '.py' },
+    { lang: 'MIPS',    code: mips_template, name: 'MIPS-IV (Simulator 0.4)', ext: '.s' },
+    { lang: 'Verilog', code: ver_template,  name: 'Verilog (iVerilog 10.0)', ext: '.v' }
 ];
